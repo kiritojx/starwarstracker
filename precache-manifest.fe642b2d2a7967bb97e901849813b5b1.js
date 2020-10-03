d39fb6813305b5b3276d669455b3323b6d6b9202
kiritojx <quainhanp24@gmail.com>
2020-10-03 17:52:47 +0700

    Updates

---
+++ precache-manifest.fe642b2d2a7967bb97e901849813b5b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b5d7e2d440551dd37f6be29476bc6f1",
+    "url": "/starwarstracker/index.html"
+  },
+  {
+    "revision": "c8d03e5b950305cf91e2",
+    "url": "/starwarstracker/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "522891cb790aed72e138",
+    "url": "/starwarstracker/static/css/main.053f9f44.chunk.css"
+  },
+  {
+    "revision": "c8d03e5b950305cf91e2",
+    "url": "/starwarstracker/static/js/2.5624172e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/starwarstracker/static/js/2.5624172e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "522891cb790aed72e138",
+    "url": "/starwarstracker/static/js/main.0a03819d.chunk.js"
+  },
+  {
+    "revision": "47c56ee1a10b696c4da4",
+    "url": "/starwarstracker/static/js/runtime-main.24040ef5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3854d1c3da9d8f50e68e8aa9aa1aaf20.js"
+  "/starwarstracker/precache-manifest.fe642b2d2a7967bb97e901849813b5b1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/starwarstracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
