7f90d42265eb32db0b3c10b56c617339ddcf3f9e
kiritojx <quainhanp24@gmail.com>
2020-10-03 17:51:44 +0700

    Updates

---
+++ precache-manifest.3854d1c3da9d8f50e68e8aa9aa1aaf20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f25afd428a59d6cb20b71e0f9ef2b36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8d03e5b950305cf91e2",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ef02e649e9fc07cb3fe7",
+    "url": "/static/css/main.053f9f44.chunk.css"
+  },
+  {
+    "revision": "c8d03e5b950305cf91e2",
+    "url": "/static/js/2.5624172e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5624172e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef02e649e9fc07cb3fe7",
+    "url": "/static/js/main.32dfcd27.chunk.js"
+  },
+  {
+    "revision": "35e7126e00fa5a02f6fe",
+    "url": "/static/js/runtime-main.341b58bb.js"
+  }
+]);
